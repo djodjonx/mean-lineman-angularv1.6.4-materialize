5f187aaeb5f708e438e73d008e25b9071042c73b
Djodjonx <djo.moutier@gmail.com>
2017-04-27 10:45:31 +0200

    Changing name of datbase

--- server/api/db.js
+++ server/api/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export default (callback) => {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/kingPong-database');
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/HackAgri-database');
     callback();
 };
